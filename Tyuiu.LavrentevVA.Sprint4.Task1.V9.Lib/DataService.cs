3acc89a0c7dd0f6b6bfd458ec7b8277b3b0293e2
dimka <dimka556.dim4@gmail.com>
2025-10-28 01:19:24 +0500

    Tas1

--- Tyuiu.LavrentevVA.Sprint4.Task1.V9.Lib/DataService.cs
+++ Tyuiu.LavrentevVA.Sprint4.Task1.V9.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.LavrentevVA.Sprint4.Task1.V9.Lib
         public int Calculate(int[] array)
         {
             // Дан одномерный целочисленный массив на 13 элементов заполненный значениями с клавиатуры в диапазоне от 3 до 8 подсчитать произведение четных элементов массива.  С клавиатуры: 6, 3, 3, 7, 7, 7, 7, 3, 3, 6, 4, 4, 8
-            int sumArray = 0;
+            int sumArray = 1;
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 == 0)
