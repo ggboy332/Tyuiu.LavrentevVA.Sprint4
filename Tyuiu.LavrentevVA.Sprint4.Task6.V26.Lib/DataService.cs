bc629a6634e24f4251aa5b93690d4eb6fdc6c360
dimka <dimka556.dim4@gmail.com>
2025-10-28 20:59:59 +0500

    Task6

---
+++ Tyuiu.LavrentevVA.Sprint4.Task6.V26.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.LavrentevVA.Sprint4.Task6.V26.Lib
+{
+    public class DataService : ISprint4Task6V26
+    {
+        public string[] Calculate(string[] array)
+        {
+            // Дан строковый массив данных ["Тюмень", "Тамбов", "Томск", "Омск", "Орёл"], используя класс Array, выведите элементы массива, длина которых больше 5 символов.
+            //  Не надо упрощать код, пойдем через трудный путь
+
+            List<string> resultList = new List<string>();
+            foreach (var str in array)
+            {
+                if (str.Length > 5)
+                {
+                    resultList.Add(str);
+                }
+            }
+            return resultList.ToArray();
+
+
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint4.Task6.V26/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.LavrentevVA.Sprint4.Task6.V26.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Tyuiu.LavrentevVA.Sprint4.Task6.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string[] strArray = new string[] { "Тюмень", "Тамбов", "Томск", "Омск", "Орёл" };
+            Console.WriteLine("Исходный массив:");
+            foreach (var str in strArray)
+            {
+                Console.WriteLine(str);
+            }
+            string[] result = ds.Calculate(strArray);
+            Console.WriteLine("число элементов массива, длина которых больше 5 символов:");
+            Console.WriteLine(result.Length);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
