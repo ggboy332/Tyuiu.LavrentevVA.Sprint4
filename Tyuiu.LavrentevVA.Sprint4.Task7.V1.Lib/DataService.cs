670cbe96709a49afe37822f30f7fae87160791fc
dimka <dimka556.dim4@gmail.com>
2025-10-28 21:49:46 +0500

    Task7

---
+++ Tyuiu.LavrentevVA.Sprint4.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.LavrentevVA.Sprint4.Task7.V1.Lib
+{
+    public class DataService : ISprint4Task7V1
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            // Дана строка из одноразрядных цифр "135792468". Преобразуйте ее в матрицу 3 на 3 и подсчитайте количество четных чисел.
+            if (value.Length != n * m)
+            {
+                throw new ArgumentException("Длина строки не соответствует размеру матрицы.");
+            }
+            int[,] matrix = new int[n, m];
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(value[index].ToString());
+                    index++;
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j] % 2 == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint4.Task7.V1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.LavrentevVA.Sprint4.Task7.V1.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Tyuiu.LavrentevVA.Sprint4.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+ 
+
+            int n = 3;
+            int m = 3;
+            string value = "135792468";
+            Console.WriteLine("Исходные данные:");
+            Console.WriteLine($"n = {n}, m = {m}, value = {value}");
+            int result = ds.Calculate(n, m, value);
+            Console.WriteLine("Количество четных чисел в матрице:");
+            Console.WriteLine(result);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
