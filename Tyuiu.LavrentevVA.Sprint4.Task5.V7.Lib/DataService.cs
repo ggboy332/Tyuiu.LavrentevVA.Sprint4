b7fe2be33acc66e4f9429c89811666b9404acaf0
dimka <dimka556.dim4@gmail.com>
2025-10-28 18:47:26 +0500

    Task5

---
+++ Tyuiu.LavrentevVA.Sprint4.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.LavrentevVA.Sprint4.Task5.V7.Lib
+{
+    public class DataService : ISprint4Task5V7
+    {
+        public int Calculate(int[,] matrix)
+        {
+            // Дан двумерный целочисленный массив 5 на 5 элементов, заполненный случайными значениями в диапазоне от -7 до 7. Найти количество отрицательных элементов.
+            int count = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+
+        }
+    }
+}
---
+++ Tyuiu.LavrentevVA.Sprint4.Task5.V7/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.LavrentevVA.Sprint4.Task5.V7.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Tyuiu.LavrentevVA.Sprint4.Task5.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int len;
+            Console.WriteLine("Введите размерность массива (5):");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[,] numsArray = new int[len, len];
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    Console.WriteLine($"Введите элемент массива [{i + 1},{j + 1}] (от -7 до 7):");
+                    numsArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    Console.Write(numsArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int result = ds.Calculate(numsArray);
+            Console.WriteLine("Количество отрицательных элементов массива:");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
